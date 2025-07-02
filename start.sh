530e0baea36b03025adff4321d861675aa6b16da
modimobeikete <1244176069@qq.com>
2025-07-02 19:56:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 # Download x-ui with error checking
 echo "Downloading easytier"
 wget -O /app/easytier.sh "https://raw.githubusercontent.com/EasyTier/EasyTier/main/script/install.sh" && bash /app/easytier.sh install --no-gh-proxy
+echo Easytier installed
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app --ssh --accept-routes
